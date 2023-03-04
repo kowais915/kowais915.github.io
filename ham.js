dd56b02861bbc4de7348d883bf87475e0e90bd23
owais <Owais>
2023-03-04 11:56:24 +0500

    the navbar is now responsive

---
+++ ham.js
@@ -0,0 +1,11 @@
+
+
+function ham(){
+    let link = document.getElementById("links");
+    if(link.style.display==="block"){
+        link.style.display ="none";
+    }else{
+        link.style.display ="black";
+    }
+}
+
